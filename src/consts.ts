8f9361b7b2c09f20e944475329c8fa7aa1855654
Riley Jones <riley@synthandhue.com>
2023-10-20 22:16:33 -0500

    new stuff

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Rileys Notes';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_DESCRIPTION = 'A scratchpad for my thoughts. Nothing fancy. Nothing mindblowing. Hopefully somehwat interesting.';
